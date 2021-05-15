84ebbdf05c5be8dcd27db13dd384b312e38a5d2e
trilloner <bo_10@ukr.net>
2021-05-15 23:44:08 +0300

    Added keywords, operators and separators

--- Lexer_JAVA/src/main/java/Utils.java
+++ Lexer_JAVA/src/main/java/Utils.java
@@ -2,12 +2,24 @@ import java.util.Arrays;
 
 public class Utils {
     static boolean isKeyword(String input) {
-        String[] keywords = new String[]{"import", "static", "enum", "class", "native", "transient", "public", "boolean", "abstract", "int"};
+        String[] keywords = new String[]{"abstract", "assert", "boolean", "break", "byte", "switch",
+                "case", "try", "catch", "finally", "char", "class", "continue", "default", "do", "double", "if", "else",
+                "enum", "extends", "final", "float", "for", "implements", "import", "instanceOf", "int", "interface", "long",
+                "native", "new", "package", "private", "protected", "public", "return", "short", "static", "strictfp",
+                "super", "synchronized", "this", "throw", "throws", "transient", "void", "volatile", "while", "goto", "const"};
         return Arrays.asList(keywords).contains(input);
     }
 
     static boolean isSeparator(char input) {
-        Character[] separators = new Character[]{'{', '}', ';', '='};
-        return Arrays.stream(separators).anyMatch(character -> input == character);
+        Character[] separators = new Character[]{'{', '}', '[', ']', ';', ':', '"', ',', '/', ' ', '\t', '(', ')', '.'};
+        return Arrays.stream(separators)
+                .anyMatch(character -> input == character);
+    }
+
+    static boolean isOperator(char input) {
+        Character[] operators = new Character[]{'-', '+', '=', '<', '>', '?', '!', '%', '&', '*', '^', '|', '/'};
+        return Arrays.stream(operators)
+                .anyMatch(character -> input == character);
+
     }
 }
